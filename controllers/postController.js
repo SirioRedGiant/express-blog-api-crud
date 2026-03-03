106146e8712d5f076f260dcd429b9295745f84fe
SirioRedGiant <Oiris1992@gmail.com>
2026-03-03 15:49:06 +0100

    FIx: add controller and clean post.js for better readibility and maintainability

---
+++ controllers/postController.js
@@ -0,0 +1,57 @@
+//note array dei post importati dalla cartella --> data
+const posts = require("../data/posts");
+
+//^ Index
+const index = (req, res) => {
+  res.json({
+    list: posts,
+  });
+};
+
+//^ Show
+const show = (req, res) => {
+  const id = parseInt(req.params.id); // l'ID  dei parametri arriva come stringa e lo trasformo in numero
+  const responseData = {
+    message: `Dettaglio del post ${id} non trovato`,
+    success: false,
+  };
+
+  const post = posts.find((post) => post.id === id); // trovo il post con lo stesso ID
+
+  if (post) {
+    return res.json(post);
+  }
+  res.status(404).json(responseData);
+};
+
+//^ Store
+const store = (req, res) => {
+  res.send("Creazione nuovo post");
+};
+
+//^ Update
+const update = (req, res) => {
+  res.send(`Modifica totale del post ${req.params.id}`);
+};
+
+//^ Destroy
+const destroy = (req, res) => {
+  const id = parseInt(req.params.id);
+  const postIndex = posts.findIndex((post) => post.id === id);
+
+  //note Se l'indice è diverso da -1, il post è stato trovato
+  if (postIndex !== -1) {
+    posts.splice(postIndex, 1); // Rimuove 1 elemento alla posizione postIndex
+    console.log(`Post ${id} eliminato. Lista aggiornata:`);
+    res.sendStatus(204); // successo (status 204 significa "No Content", operazione riuscita)
+  } else {
+    //note Se non lo trova, errore 404
+    res.status(404).json({
+      error: "Post non trovato",
+      message: `Impossibile da eliminare, il post ${id} non esiste.`,
+    });
+  }
+};
+
+module.exports = { index, show, store, update, destroy };
+// esportazione delle funzione singolarmente( sono in un oggetto... quindi "chiave : valore" omesso perchè identico)
--- routers/posts.js
+++ routers/posts.js
@@ -1,60 +1,23 @@
 const express = require("express");
 const router = express.Router(); // crea un server per i post
 
-//note array dei post importati dalla cartella --> data
-const posts = require("../data/posts");
+//! importazione delle funzione del controller
+const postController = require("../controllers/postController");
 
 //^ Index --> Lista dei post (GET)
-router.get("/", (req, res) => {
-  res.json({
-    list: posts,
-  });
-});
+router.get("/", postController.index);
 
 //^ Show --> Dettagli di un singolo post (GET)
-router.get("/:id", (req, res) => {
-  const id = parseInt(req.params.id); // l'ID  dei parametri arriva come stringa e lo trasformo in numero
-  const responseData = {
-    message: `Dettaglio del post ${id} non trovato`,
-    success: false,
-  };
-
-  const post = posts.find((post) => post.id === id); // trovo il post con lo stesso ID
-
-  if (post) {
-    return res.json(post);
-  }
-  res.status(404).json(responseData);
-});
+router.get("/:id", postController.show);
 
 //^ Store --> Creazione di un nuovo post (POST)
-router.post("/", (req, res) => {
-  res.send("Creazione nuovo post");
-});
+router.post("/", postController.store);
 
 //^ Update --> Modifica totale di un post (PUT)
-router.put("/:id", (req, res) => {
-  res.send(`Modifica totale del post ${req.params.id}`);
-});
+router.put("/:id", postController.update);
 
 //^ Destroy --> Eliminazione di un post (DELETE)
-router.delete("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const postIndex = posts.findIndex((post) => post.id === id);
-
-  //note Se l'indice è diverso da -1, il post è stato trovato
-  if (postIndex !== -1) {
-    posts.splice(postIndex, 1); // Rimuove 1 elemento alla posizione postIndex
-    console.log(`Post ${id} eliminato. Lista aggiornata:`);
-    res.sendStatus(204); // successo (status 204 significa "No Content", operazione riuscita)
-  } else {
-    //note Se non lo trova, errore 404
-    res.status(404).json({
-      error: "Post non trovato",
-      message: `Impossibile da eliminare, il post ${id} non esiste.`,
-    });
-  }
-});
+router.delete("/:id", postController.destroy);
 
 //^ Esportazione del router
 module.exports = router;
