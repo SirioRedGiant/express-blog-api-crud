9b274f94efbe546c96c6156b5cfdd721de6da3ed
SirioRedGiant <Oiris1992@gmail.com>
2026-03-05 14:15:19 +0100

    Add: supplementary comment

--- app.js
+++ app.js
@@ -22,11 +22,22 @@ app.use(notFound);
 //! errorsHandler deve essere sempre l'ULTIMO
 app.use(errorsHandler);
 
-/** //^ Da scrivere meglio
+/**
  * La Cascata: Se errorsHandler fosse sopra notFound, quando un utente cerca una rotta inesistente, Express passerebbe sopra l'errorHandler (perché non c'è un errore di
  * codice, solo una rotta mancante) e si ferma al notFound.
  * La Rete di Sicurezza: Se invece avviene un errore nel tuo codice (un 500), Express "salta" tutto quello che incontra (compreso il 404) per precipitare nell'unico middleware
  * che ha 4 parametri: l'errorsHandler. Express Error Handling
+ */
+
+/** //! --- ORDINE DEI MIDDLEWARE (Fondamentale) --- 
+
+ * 1. Rotte --> Express cerca un match dall'alto verso il basso.
+
+ * 2. 404 notFound: Se nessuna rotta che è sopra risponde, la richiesta "si ferma" qui. Gestisce le richieste che si sono perse.
+
+ * 3. 500 errorHandler: Deve essere l'ULTIMO in assoluto. Express lo attiva solo se un middleware precedente "crasha" o chiama next(err). 
+ *    Essendo l'unico con 4 parametri (err, req, res, next), funge da rete di sicurezza finale.
+
  */
 
 app.listen(port, () => {
